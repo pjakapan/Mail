788640d7da7b9e505eb6767f0a4dbcce5616c10f
pjakapan <j.petchan@togetherteam.co.th>
2017-11-20 13:17:42 +0700

    Multimail

--- js/loadJsonForm.js
+++ js/loadJsonForm.js
@@ -4,6 +4,7 @@
 // -----------------------------------------------------------------------------------------------------------------------------
 
 var arrForm = loadForm();
+var arrForm2 = loadForm2();
 
 function loadForm() {
 
@@ -20,3 +21,20 @@ function loadForm() {
     });
     return json;
 }
+
+function loadForm2() {
+    
+        var json = null;
+        var getsetting = {
+            'async': false,
+            'global': false,
+            'url': "./data/mailform.json",
+            'dataType': "json",
+            "method": "GET"
+        }
+        $.ajax(getsetting).done(function (data) {
+            json = data;
+        });
+        return json;
+    }
+    
